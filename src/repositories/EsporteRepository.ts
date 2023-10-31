1b12db8983db77077fac7d909d762c9649d90eca
estanho <estanhoeu@gmail.com>
2023-10-31 20:01:52 -0300

    Alterando ordem do resultado de cidades

--- src/repositories/EsporteRepository.ts
+++ src/repositories/EsporteRepository.ts
@@ -2,7 +2,11 @@ import { prismaClient as prisma } from '../database/prismaClient';
 
 export class EsporteRepository {
   async getAll() {
-    const esporte = await prisma.esporte.findMany();
+    const esporte = await prisma.esporte.findMany({
+      orderBy: {
+        nome: 'desc',
+      },
+    });
     return { esportes: esporte };
   }
 }
